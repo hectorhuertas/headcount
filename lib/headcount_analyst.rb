332ebef6a91ee9e9e4158ca5264b46914ec1dcd5
Hector Huertas <hectorhuertas@gmail.com>
2015-11-12 13:42:44 -0700

    analyst improvement

--- lib/headcount_analyst.rb
+++ lib/headcount_analyst.rb
@@ -10,22 +10,21 @@ class HeadcountAnalyst
     dist_1 = district_repository.find_by_name(d1_name)
     dist_2 = district_repository.find_by_name(d2_name)
 
-    # rates_1 = dist_1.enrollment.kindergarten_participation_by_year
-    # rates_2 = dist_2.enrollment.kindergarten_participation_by_year
-
-    kindergarten_participation_rate_variation_of_enrollments(dist_1.enrollment, dist_2.enrollment)
+    avg_1 = district_average(dist_1,{area: :enrollment, type: :kindergarten_participation_by_year})
+    avg_2 = district_average(dist_2,{area: :enrollment, type: :kindergarten_participation_by_year})
+    avg_1/avg_2
   end
 
-  def kindergarten_participation_rate_variation_of_enrollments(e1, e2)
-    avg1 = kindergarten_participation_average(e1)
-    avg2 = kindergarten_participation_average(e2)
-    avg1 / avg2
-  end
-
-  def kindergarten_participation_average(e)
-    sum   = e.kindergarten_participation_by_year.values.reduce(:+)
-    count = e.kindergarten_participation_by_year.length
+  #info = {area: :enrollment, type: :kindergarten }
+  def district_average(district, info)
+    data = load_district_data(district, info)
+    sum = data.values.reduce(:+)
+    count = data.size
     sum / count
+  end
+
+  def load_district_data(district, info)
+    district.send(info[:area]).send(info[:type])
   end
 
   def kindergarten_participation_rate_variation_trend(d1_name, options)
@@ -35,25 +34,35 @@ class HeadcountAnalyst
 
     # rates_1 = dist_1.enrollment.kindergarten_participation_by_year
     # rates_2 = dist_2.enrollment.kindergarten_participation_by_year
-
-    kindergarten_trend_for_enrollments(dist_1.enrollment, dist_2.enrollment)
-  end
-
-  def kindergarten_trend_for_enrollments(e1, e2)
-    rates_1 = e1.kindergarten_participation_by_year
-    rates_2 = e2.kindergarten_participation_by_year
-    years = rates_1.keys & rates_2.keys
-
-    year_rates_1 ={}
-    years.each {|year| year_rates_1[year]=rates_1[year]}
-    year_rates_2 ={}
-    years.each {|year| year_rates_2[year]=rates_2[year]}
     # binding.pry
-    trends = year_rates_1.merge(year_rates_2) do |year, av1, av2|
-      av2/av1
-     end
-    # end
 
-    trends.sort.to_h
+    trend_1 = dist_1.enrollment.kindergarten_participation_by_year
+    trend_2 = dist_2.enrollment.kindergarten_participation_by_year
+    # kindergarten_trend_for_enrollments(dist_1.enrollment, dist_2.enrollment)
+    compare_trends(trend_1, trend_2)
   end
+
+  def compare_trends(trend_1,trend_2)
+    common = trend_1.keys & trend_2.keys
+
+    common_trend_1 ={}
+    common.each {|year| common_trend_1[year]=trend_1[year]}
+    common_trend_2 ={}
+    common.each {|year| common_trend_2[year]=trend_2[year]}
+     common_trend_1.merge(common_trend_2) do |year, av1, av2|
+      av2/av1
+    end.sort.to_h
+  end
+
+  # def kindergarten_participation_against_high_school_graduation(d_name)
+  #   dist = district_repository.find_by_name(d1_name)
+  #   kinder_variation = kindergarten_participation_average(dist.enrollment) / statewide_average(kindergarten)
+  #   graduation_variation = district_graduation / statewide_average(graduation)
+  #
+  #   variance(kinder_variation,graduation)
+  # end
+  #
+  # def variance(vaue1, value2)
+  #   value2 / value1
+  # end
 end
--- test/headcount_analyst_test.rb
+++ test/headcount_analyst_test.rb
@@ -8,61 +8,128 @@ class HeadcountAnalystTest < Minitest::Test
     # EnrollmentRepo needs Enrollment
     # Statewide testing needs StatewideTesting
     #Economic needs EconomicProfile
+
+  def test_it_exist
+    assert HeadcountAnalyst
+  end
+
+  def test_it_takes_a_district_repository
+    dr = DistrictRepository.new
+    assert ha = HeadcountAnalyst.new(dr)
+  end
+
   def test_it_compares_kindergarten_rates_across_districts
     e1 = Enrollment.new({name: "Dist_1",
                         kindergarten_participation: {2010 => 1.0}})
     e2 = Enrollment.new({name: "Dist_2",
-                        kindergarten_participation: {2010 => 1.0}})
-
+                        kindergarten_participation: {2010 => 2.0}})
     er = EnrollmentRepo.new
     er.add_enrollments([e1,e2])
-
-    # skip
     dr = DistrictRepository.new
-
     dr.load_repos({enrollment: er})
 
     ha = HeadcountAnalyst.new(dr)
     dist_1 = "Dist_1"
     dist_2 = "Dist_2"
     rate = ha.kindergarten_participation_rate_variation(dist_2, :against => dist_1)
-    assert_equal 1.0, rate
+    assert_equal 2.0, rate
   end
 
-  def test_it_compares_enrollment_rates_between_enrollments
-    e1 = Enrollment.new({name: "Dist_1",
-                        kindergarten_participation: {2010 => 1.0, 2011 => 2.0}})
-    e2 = Enrollment.new({name: "Dist_2",
-                        kindergarten_participation: {2010 => 2.0, 2011 => 3.0}})
-
-    dr = DistrictRepository.new
-
-    ha = HeadcountAnalyst.new(dr)
-    average = ha.kindergarten_participation_rate_variation_of_enrollments(e1,e2)
-    assert_equal 0.6, average
-  end
-
-  def test_it_averages_enrollments
-    dr = DistrictRepository.new
-
-    ha = HeadcountAnalyst.new(dr)
-    e1 = Enrollment.new({name: "Dist_1",
-                        kindergarten_participation: {2010 => 2.0, 2011 => 3.0}})
-    average = ha.kindergarten_participation_average(e1)
-    assert_equal 2.5, average
-  end
-
-  def test_it_calculates_trends
+  def test_it_calculates_kindergarten_participation_trends
     e1 = Enrollment.new({name: "Dist_1",
                         kindergarten_participation: {2010 => 1.0, 2012 => 2.0, 2014 => 2.0}})
     e2 = Enrollment.new({name: "Dist_2",
                         kindergarten_participation: {2010 => 2.0, 2011 => 3.0, 2014 => 1.0}})
-
+    er = EnrollmentRepo.new
+    er.add_enrollments([e1,e2])
     dr = DistrictRepository.new
-
+    dr.load_repos({enrollment: er})
     ha = HeadcountAnalyst.new(dr)
     expected = {2010 => 2.0, 2014 => 0.5}
-    # trend =ha.kindergarten_participation_rate_variation_trend("Dist_1", against: "Dist_2")
-    assert_equal expected, ha.kindergarten_trend_for_enrollments(e1,e2)
+
+    assert_equal expected, ha.kindergarten_participation_rate_variation_trend('Dist_1', :against => 'Dist_2')
   end
+
+
+  def test_it_returns_average
+    dr = DistrictRepository.new
+    ha = HeadcountAnalyst.new(dr)
+
+    e1 = Enrollment.new({name: "Dist_1",
+                        kindergarten_participation: {2010 => 1.0, 2012 => 2.0}})
+
+    d = District.new(name:"Dist_1")
+    d.enrollment = e1
+
+    assert_equal 1.5, ha.district_average(d, {area: :enrollment, type: :kindergarten_participation_by_year})
+  end
+
+  def test_it_loads_data_for_a_given_district_and_info
+    dr = DistrictRepository.new
+    ha = HeadcountAnalyst.new(dr)
+
+    e1 = Enrollment.new({name: "Dist_1",
+                        kindergarten_participation: {2010 => 1.0, 2012 => 2.0}})
+
+    d = District.new(name:"Dist_1")
+    d.enrollment = e1
+    expected = {2010 => 1.0, 2012 => 2.0}
+    assert_equal expected, ha.load_district_data(d,{area: :enrollment, type: :kindergarten_participation_by_year} )
+  end
+
+  def test_it_compares_trends
+    trend_1 = {2010 => 1.0, 2012 => 2.0, 2014 => 2.0}
+    trend_2 = {2010 => 2.0, 2011 => 3.0, 2014 => 1.0}
+
+    expected = {2010 => 2.0, 2014 => 0.5}
+
+    dr = DistrictRepository.new
+    ha = HeadcountAnalyst.new(dr)
+    assert_equal expected, ha.compare_trends(trend_1, trend_2)
+  end
+
+  # def test_it_compares_enrollment_rates_between_enrollments
+  #   e1 = Enrollment.new({name: "Dist_1",
+  #                       kindergarten_participation: {2010 => 1.0, 2011 => 2.0}})
+  #   e2 = Enrollment.new({name: "Dist_2",
+  #                       kindergarten_participation: {2010 => 2.0, 2011 => 3.0}})
+  #
+  #   dr = DistrictRepository.new
+  #
+  #   ha = HeadcountAnalyst.new(dr)
+  #   average = ha.kindergarten_participation_rate_variation_of_enrollments(e1,e2)
+  #   assert_equal 0.6, average
+  # end
+
+  # def test_it_averages_enrollments
+  #   dr = DistrictRepository.new
+  #
+  #   ha = HeadcountAnalyst.new(dr)
+  #   e1 = Enrollment.new({name: "Dist_1",
+  #                       kindergarten_participation: {2010 => 2.0, 2011 => 3.0}})
+  #   average = ha.kindergarten_participation_average(e1)
+  #   assert_equal 2.5, average
+  # end
+
+
+  # def test_it_calculates_kindergarten_participation_trends_by_enroll
+  #   e1 = Enrollment.new({name: "Dist_1",
+  #                       kindergarten_participation: {2010 => 1.0, 2012 => 2.0, 2014 => 2.0}})
+  #   e2 = Enrollment.new({name: "Dist_2",
+  #                       kindergarten_participation: {2010 => 2.0, 2011 => 3.0, 2014 => 1.0}})
+  #
+  #   dr = DistrictRepository.new
+  #   ha = HeadcountAnalyst.new(dr)
+  #   expected = {2010 => 2.0, 2014 => 0.5}
+  #
+  #   assert_equal expected, ha.kindergarten_trend_for_enrollments(e1,e2)
+  # end
+
+  def test_it_compares_kindergarten_and_high_school_enrollments
+    skip
+    dr = DistrictRepository.new
+    ha = HeadcountAnalyst.new(dr)
+    assert_equal 1.2, ha.kindergarten_participation_against_high_school_graduation('ACADEMY 20')
+  end
+
 end
