3f39ab30cf6986ca6f667796466d95c05d2fe82a
Hector Huertas <hectorhuertas@gmail.com>
2015-11-17 15:15:28 -0700

    more it2

--- lib/st_parser.rb
+++ lib/st_parser.rb
@@ -1,5 +1,5 @@
 require 'csv'
-require 'pry'
+require 'pry-rescue'
 
 module StParser
 
@@ -16,9 +16,9 @@ module StParser
     raw_data.reduce({}) { |result, (k, v)| result[k] = { key_info => v }; result }
   end
   #
-  # def self.wrap_name(hash)
-  #   c = hash.map { |k, v| { name: k }.merge v }
-  # end
+  def self.wrap_name(hash)
+    c = hash.map { |k, v| { name: k }.merge v }
+  end
   #
   # def self.is_not_a_number?(string)
   #   string.start_with?("#", "N")
@@ -33,11 +33,32 @@ module StParser
   # end
 
   def self.st_test(data)
-    # parsed_data = data.keys.reduce({}) do |result, filetype|
-    #   new_data = send(filetype, data[filetype])
-    #   result.merge(new_data) { |_k, v1, v2| v1.merge v2 }
-    # end
-    # wrap_name(parsed_data)
+    third_grade = third_grade(data[:third_grade], :third_grade)
+    eighth_grade = eighth_grade(data[:eighth_grade], :eighth_grade)
+    proficiency = avg_proficiency({math: data[:math], reading: data[:reading], writing: data[:writing]})
+    e =[third_grade, eighth_grade, proficiency].reduce do |result, hash|
+      result.merge(hash){|k,v1,v2|v1.merge(v2)}
+    end
+    wrap_name(e)
+  end
+
+  def self.third_grade(filename, filetype)
+    data = third_and_eighth_grade(filename)
+    wrap(data, filetype)
+  end
+
+  def self.eighth_grade(filename, filetype)
+    data = third_and_eighth_grade(filename)
+    wrap(data, filetype)
+  end
+
+  def self.avg_proficiency(data)
+    math = race(data[:math], :math)
+    reading = race(data[:reading], :reading)
+    writing = race(data[:writing], :writing)
+    e = [math,reading,writing].reduce{|result,hash|
+    merge_deeper(result,hash)}
+    # binding.pry
   end
 
   def self.third_and_eighth_grade(filename)
@@ -48,9 +69,33 @@ module StParser
     raw_data = merge_deep(lines)
   end
 
+  def self.race(filename, filetype)
+    lines = CSV.open(filename, headers: true, header_converters: :symbol).map do |row|
+      {row[:location].upcase => {row[:race_ethnicity].to_sym => {row[:timeframe].to_i =>{ filetype => row[:data].to_f.round(3)}}}}
+    end.compact
+    raw_data = merge_years(lines)
+    # binding.pry
+  end
+
   def self.merge_deep(lines)
     lines.reduce{|result,line| result.merge(line){|k,v1,v2|
       v1.merge(v2){|k,v1,v2| v1.merge v2}
       }}
+  end
+
+  def self.merge_years(lines)
+    lines.reduce{|result,line| result.merge(line){|k,v1,v2|
+      v1.merge(v2){|k,v1,v2| v1.merge(v2){|k,v1,v2|
+        # binding.pry
+        v1.merge(v2)
+        }}
+      }}
+  end
+
+  def self.merge_deeper(h1,h2)
+    h1.merge(h2){|k,v1,v2|
+       v1.merge(v2){|k,v1,v2|
+         v1.merge(v2){|k,v1,v2|
+           v1.merge(v2)}}}
   end
 end
