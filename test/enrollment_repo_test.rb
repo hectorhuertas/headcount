6552fce96b5a00e001033bb28e31c8b5c8391813
Hector Huertas <hectorhuertas@gmail.com>
2015-11-17 19:29:37 -0700

    more tests

--- test/enrollment_repo_test.rb
+++ test/enrollment_repo_test.rb
@@ -42,5 +42,35 @@ class EnrollmentRepositoryTest < Minitest::Test
     assert_equal "COLORADO", er.find_by_name("Colorado").name
   end
 
+  def test_creates_new_enrollments
+    er =EnrollmentRepository.new
+    test_data = [{name: 'tom'}]
+    er.create_new_enrollments(test_data)
+    assert_equal 'TOM', er.find_by_name('tom').name
+  end
 
+  def test_creates_another_enrollment
+    er =EnrollmentRepository.new
+    test_data = [{name: 'yay'}]
+    er.create_new_enrollments(test_data)
+    assert_equal 'YAY', er.find_by_name('yay').name
+  end
+
+  def test_creates_several_enrollment
+    er =EnrollmentRepository.new
+    test_data = [{name: 'tom'}, {name: 'hector'}]
+    er.create_new_enrollments(test_data)
+    assert_equal 'TOM', er.find_by_name('tom').name
+    assert_equal 'HECTOR', er.find_by_name('hector').name
+  end
+
+  def test_find_by_name_case_insensitive
+    er = EnrollmentRepository.new([Enrollment.new(name: 'fake')])
+    assert_equal 'FAKE', er.find_by_name('fake').name
+  end
+
+  def test_find_by_another_name_case_insensitive
+    er = EnrollmentRepository.new([Enrollment.new(name: 'NOT')])
+    assert_equal 'NOT', er.find_by_name('noT').name
+  end
 end
--- test/statewide_test_repository_test.rb
+++ test/statewide_test_repository_test.rb
@@ -7,12 +7,27 @@ class StatewideTestRepositoryTest < Minitest::Test
   end
 
   def test_it_loads_3rd_grade
-    skip
     str = StatewideTestRepository.new
     str.load_data(statewide_testing: {
-                    third_grade: './data/3rd.csv'
+                    third_grade: './test/data/3g.csv'
                   })
-    assert_equal 'dd', str.find_by_name('coloRADo')
+    assert_equal 'COLORADO', str.find_by_name('coloRADo').name
+  end
+
+  def test_it_loads_data
+    str = StatewideTestRepository.new
+    str.load_data(statewide_testing: {
+                    third_grade: './test/data/3g.csv',
+                    eighth_grade: './test/data/8g.csv',
+                    math: './test/data/math.csv',
+                    reading: './test/data/reading.csv',
+                    writing: './test/data/writing.csv' })
+    assert str.find_by_name('ACADEMY 20')
+    assert str.find_by_name('ACADEMY 20')
+    assert str.find_by_name('COLORADO')
+    assert str.find_by_name('Madrid')
+    assert str.find_by_name('London')
+    assert str.find_by_name('Academy 20')
   end
 
   def test_find_by_name_case_insensitive
@@ -27,46 +42,32 @@ class StatewideTestRepositoryTest < Minitest::Test
 
   def test_creates_new_STtest
     str = StatewideTestRepository.new
-    tests_data = [{name: "yeah"}]
+    tests_data = [{ name: 'yeah' }]
     str.create_new_STtests(tests_data)
-    assert_equal "YEAH", str.find_by_name("yeah").name
+    assert_equal 'YEAH', str.find_by_name('yeah').name
   end
 
   def test_creates_another_new_STtest
     str = StatewideTestRepository.new
-    tests_data = [{name: "bob"}]
+    tests_data = [{ name: 'bob' }]
     str.create_new_STtests(tests_data)
-    assert_equal "BOB", str.find_by_name("bob").name
+    assert_equal 'BOB', str.find_by_name('bob').name
   end
 
   def test_creates_several_STtest
     str = StatewideTestRepository.new
-    tests_data = [{name: "yeah"},{name: "bob"} ]
+    tests_data = [{ name: 'yeah' }, { name: 'bob' }]
     str.create_new_STtests(tests_data)
-    assert_equal "YEAH", str.find_by_name("yeah").name
-    assert_equal "BOB", str.find_by_name("bob").name
+    assert_equal 'YEAH', str.find_by_name('yeah').name
+    assert_equal 'BOB', str.find_by_name('bob').name
   end
 
   def test_creates_different_several_STtest
     str = StatewideTestRepository.new
-    tests_data = [{name: "one"},{name: "two"} ]
+    tests_data = [{ name: 'one' }, { name: 'two' }]
     str.create_new_STtests(tests_data)
-    assert_equal "ONE", str.find_by_name("one").name
-    assert_equal "TWO", str.find_by_name("two").name
+    assert_equal 'ONE', str.find_by_name('one').name
+    assert_equal 'TWO', str.find_by_name('two').name
   end
 
-  def test_it_loads_data
-    skip
-    str = StatewideTestRepository.new
-    str.load_data({
-      :statewide_testing => {
-      :third_grade => "./data/3rd grade students scoring proficient or above on the CSAP_TCAP.csv",
-      :eighth_grade => "./data/8th grade students scoring proficient or above on the CSAP_TCAP.csv",
-      :math => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Math.csv",
-      :reading => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Reading.csv",
-      :writing => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Writing.csv"
-  }
-})
-str = str.find_by_name("ACADEMY 20")
-  end
 end
